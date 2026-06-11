5860c83f19409843d0f4f57f8e05c9ff6045e926
bcarcamo247 <bcarcamo247@gmail.com>
2026-06-11 12:12:41 -0400

    add registration and login routes to users api

--- api/users.js
+++ api/users.js
@@ -1,11 +1,17 @@
 import express from "express";
 const router = express.Router();
 export default router;
-
+/* Importing all functions from relevant files so
+the API knows the data to access and use */
 import { createUser, getUserByUsernameAndPassword } from "#db/queries/users";
 import requireBody from "#middleware/requireBody";
 import { createToken } from "#utils/jwt";
-
+/*Route handler used to created an account for user
+ requireBody ensures username and pw exist
+ create user and a token for correct login credentials
+ send a 201 response that token was successfully created and 
+ returned
+ */
 router.post(
     "/register",
     requireBody(["username", "password"]),
@@ -18,7 +24,11 @@ router.post(
     },
 );
 
-
+/* Finds user with the correct username and password
+token is created for the user
+if username and password are incorrect return authentication error 401
+return the created token
+ */
 router.post(
     "/login", requireBody(["username", "password"]), async (req, res) => {
         const { username, password } = req.body;
